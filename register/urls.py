40cfbfd0436e49f762f3bc2ce70c299bb754c96e
bbelderbos <bobbelderbos@gmail.com>
2017-07-31 03:42:32 +0200

    another try https://docs.djangoproject.com/en/1.11/ref/views/#django.views.static.serve

--- register/urls.py
+++ register/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
+from django.views.static import serve
 
 from . import views
 
@@ -27,5 +27,8 @@ urlpatterns = [
 ]
 
 if not settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
+    urlpatterns += [
+        url(r'^static/(?P<path>.*)$', serve, {
+            'document_root': settings.STATIC_ROOT,
+        }),
+    ]
