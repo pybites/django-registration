fa52a9b95abda3d90d14d66e5d3055e70bbfb598
bbelderbos <bobbelderbos@gmail.com>
2017-07-31 03:26:58 +0200

    https://stackoverflow.com/questions/9047054/heroku-handling-static-files-in-django-app urls tweak

--- register/urls.py
+++ register/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
 
 from . import views
 
@@ -23,3 +24,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += url('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
